8cedcff25800946e5c57909754aee3d2570b720f
Martin Maly <martinmaly66@hotmail.com>
2018-08-23 22:05:11 -0400

    Commit.

--- PartyTime/ViewControllers/ViewController.swift
+++ PartyTime/ViewControllers/ViewController.swift
@@ -11,9 +11,9 @@ import FirebaseAuth
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
+
+   override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
