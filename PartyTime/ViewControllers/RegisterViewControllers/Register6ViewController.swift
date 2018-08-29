0965ad388157d8d7b3edd849105738b0a62aadbb
Martin Maly <martinmaly66@hotmail.com>
2018-08-29 18:29:04 -0400

    Changed registering account.

---
+++ PartyTime/ViewControllers/RegisterViewControllers/Register6ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Register6ViewController.swift
+//  PartyTime
+//
+//  Created by Martin Maly on 2018-08-29.
+//  Copyright © 2018 Martin Maly. All rights reserved.
+//
+
+import UIKit
+
+class Register6ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
