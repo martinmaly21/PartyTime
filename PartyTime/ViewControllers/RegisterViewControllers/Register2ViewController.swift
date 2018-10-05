181d8784bf5f5166b650e70625de1b584c5a5ffe
Martin Maly <martinmaly66@hotmail.com>
2018-10-05 12:25:56 -0400

    Commit.

--- PartyTime/ViewControllers/RegisterViewControllers/Register2ViewController.swift
+++ PartyTime/ViewControllers/RegisterViewControllers/Register2ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class Register2ViewController: UIViewController {
-
+    let formatter = DateFormatter()
+    @IBOutlet weak var birthdayText: HoshiTextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+ 
+        
+        birthdayText.text = formatter.string(from: datePicker.date)
+        
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Write code so that user can not pick date ahead of today's date
+        //Write code to ensure they're 19
+        datePicker.setValue(UIColor.white, forKey: "textColor")
+        formatter.dateFormat = "MMMM dd, yyyy"
+        
+        birthdayText.text = formatter.string(from: datePicker.date)
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +35,5 @@ class Register2ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
